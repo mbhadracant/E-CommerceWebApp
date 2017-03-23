0f99034171792ec6cd067e099dfe10127e10ed60
Kowrishankar Kaneshamoorthy <K.Kowrishankar@yahoo.com>
2017-03-23 22:01:41 +0000

    Update views.py

--- MKATechnologies/product/views.py
+++ MKATechnologies/product/views.py
@@ -2,15 +2,18 @@ import csv
 
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import Products
-from .models import Orders
+from .models import Product
+from .models import Category
+from .models import Subcategory
+from .models import Order
 from .serializers import  ProductSerializer
+from .serializers import  OrderSerializer
 from rest_framework.response import Response
 import os
 
 class search(APIView):
     def get(self, request):
-        allProducts = Products.objects.all()
+        allProducts = Product.objects.all()
         if request.GET.get('category') is not None:
             allProducts = allProducts.filter(product_category=request.GET.get('category'))
         if request.GET.get('brand') is not None:
@@ -25,7 +28,7 @@ class search(APIView):
 #Gets all the products
 class ProductList(APIView):
     def get(self, request):
-        products = Products.objects.all()
+        products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
@@ -35,81 +38,73 @@ class ProductList(APIView):
 #Get product details given a product id
 class ProductDetails(APIView):
     def get(self, request):
-        product = Products.objects.get(product_id=request.GET.get(''))
+        product = Product.objects.get(product_id=request.GET.get(''))
         serializer = ProductSerializer(product, many=False)
         return Response(serializer.data)
 
 #Get products given the category
 class ProductCategory(APIView):
     def get(self, request):
-        products = Products.objects.filter(product_category=request.GET.get(''))
+        products = Product.objects.filter(product_category=request.GET.get(''))
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
 #Get all the products given the brand
 class ProductBrand(APIView):
     def get(self, request):
-        products = Products.objects.filter(product_make=request.GET.get(''))
+        products = Product.objects.filter(product_make=request.GET.get(''))
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
 class OrderList(APIView):
     def get(self, request):
-        orders = Orders.objects.all()
+        orders = Order.objects.all()
         serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
 
 class OrderDetails(APIView):
     def get(self, request):
-        order = Orders.objects.get(order_id=request.GET.get(''))
+        order = Order.objects.get(order_id=request.GET.get(''))
         serializer = OrderSerializer(order, many=False)
         return Response(serializer.data)
 
 class UserOrderDetails(APIView):
     def get(self, request):
-        orders = Orders.objects.filter(user_id=request.GET.get(''))
+        orders = Order.objects.filter(user_id=request.GET.get(''))
         serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
 
-#Get user details given user id
-class UserDetails(APIView):
+# #Get user details given user id
+# class UserDetails(APIView):
+#     def get(self, request):
+#         user = Users.objects.get(username=request.GET.get(''))
+#         serializer = UserSerializer(user, many=False)
+#         return Response(serializer.data)
+
+class loadCategory(APIView):
     def get(self, request):
-        user = Users.objects.get(username=request.GET.get(''))
-        serializer = UserSerializer(user, many=False)
-        return Response(serializer.data)
+        BASE = os.path.dirname(os.path.abspath(__file__))
+        f = open(os.path.join(BASE, "category.txt"))
+        for record in f:
+            data = record.split(':')
+            _, created = Category.objects.get_or_create(
+                category=data[0]
+            )
+            subcate = data[1].split(',')
+            for subcategory in subcate:
+                _, sub = Subcategory.objects.get_or_create(
+                    category_id=getattr(created, 'category_id'),
+                    subcategory=subcategory[0]
+                )
+        return Response("")
 
 class load(APIView):
     def get(self, request):
         BASE = os.path.dirname(os.path.abspath(__file__))
-        # with open(os.path.join(BASE, "data.txt")) as f:
-        #     reader = csv.reader(f)
-        #     for row in reader:
-        #         _, created = Products.objects.get_or_create(
-        #             product_name=row[0],
-        #             product_make=row[1],
-        #             product_category=row[2],
-        #             product_sub_category=row[3],
-        #             price=row[4],
-        #             colour=row[5],
-        #             description=row[6], image_link=row[7], quantity=row[8]
-        #         )
-
         f = open(os.path.join(BASE, "data.txt"))
         for record in f:
             data = record.split(';')
-            # _, created = Products.objects.get_or_create (
-            #     product_name=record[0],
-            #     product_make=record[1],
-            #     product_category=record[2],
-            #     product_subcategory=record[3],
-            #     price=record[4],
-            #     description=record[5],
-            #     image_link=record[6],
-            #     quantity=record[7]
-            # )
-        #
-
-            _, created = Products.objects.get_or_create(
+            _, created = Product.objects.get_or_create(
                 product_name=data[0],
                 product_make=data[1],
                 product_category=data[2],
