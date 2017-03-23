520e73292b89750a1f98a079f558b4d36341b470
kowrishankar <k.kaneshamoorthy@se13.qmul.ac.uk>
2017-03-23 18:24:36 +0000

    removed product_option table and renamed fields and table names

--- MKATechnologies/product/models.py
+++ MKATechnologies/product/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from django.db import models
 from accounts.models import Users
 
-class Products(models.Model):
+class Product(models.Model):
     product_id = models.AutoField(primary_key = True)
     product_name = models.CharField(max_length=200)
     product_make = models.CharField(max_length=200)
@@ -13,13 +13,7 @@ class Products(models.Model):
     image_link = models.CharField(max_length=200)
     quantity = models.CharField(max_length=200)
 
-class Product_option(models.Model):
-    option_id = models.AutoField(primary_key=True)
-    product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
-    option = models.CharField(max_length=200)
-    price = models.IntegerField()
-
-class Orders(models.Model):
+class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     product_id= models.ForeignKey(Products, on_delete=models.CASCADE)
@@ -28,9 +22,11 @@ class Orders(models.Model):
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
-    category = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
 class Subcategory(models.Model):
     subcategory_id = models.AutoField(primary_key=True)
     category_id = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
-    subcategory = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    subcategory_image = models.CharField(max_length=200)
+
